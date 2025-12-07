f583985fe9b9ffa7b04023d974438ab541c18521
Naga sai Krishna Grandham <nagasaikrishna01@gmail.com>
2025-12-07 10:03:25 +0530

    Updated pc

--- single_cycle_cpu/single_cycle_cpu.srcs/sources_1/new/pc.v
+++ single_cycle_cpu/single_cycle_cpu.srcs/sources_1/new/pc.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.12.2025 22:02:39
-// Design Name: 
-// Module Name: pc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module pc(input clk,
 input reset,
