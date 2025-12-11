4875d3b49cd259d16e4f07aa95177d3cd4ed5aab
Naga sai Krishna Grandham <nagasaikrishna01@gmail.com>
2025-12-11 07:51:38 +0530

    Updated program counter logic

--- single_cycle_cpu/single_cycle_cpu.srcs/sources_1/new/pc.v
+++ single_cycle_cpu/single_cycle_cpu.srcs/sources_1/new/pc.v
@@ -1,14 +1,15 @@
-`timescale 1ns / 1ps
+module pc (
+    input         clk,
+    input         reset,
+    input  [31:0] pc_next,    // from next PC logic
+    output reg [31:0] pc_out
+);
 
-module pc(input clk,
-input reset,
-output reg [31:0]pc_out
-    );
-    always@(posedge clk or posedge reset)
-    begin
-    if(reset)
-    pc_out<=0;
-    else
-    pc_out<=pc_out+4;
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            pc_out <= 0;
+        else
+            pc_out <= pc_next;
     end
+
 endmodule
